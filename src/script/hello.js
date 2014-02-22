41c288632451788c98808bfcfd20e787d67221b1
Sven Helmberger <fforw@gmx.de>
2014-02-22 13:34:58 +0100

    Implement same with CDN react
    Remove all requires for React and move it to dev dependencies so it will
    still be there for the build process.

--- src/script/hello.js
+++ src/script/hello.js
@@ -1,7 +1,5 @@
 /** @jsx React.DOM */
 
-var React = require("react");
-
 var Hello = React.createClass({
     render: function ()
     {
--- src/script/main.js
+++ src/script/main.js
@@ -1,6 +1,5 @@
 /** @jsx React.DOM */
 
-var React = require("react");
 var Hello = require("./hello");
 
 React.renderComponent(<Hello/>, document.getElementById("container"), function ()
